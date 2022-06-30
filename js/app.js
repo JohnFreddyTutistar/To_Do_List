df06abb0d368d6c8c509aaa89e08217cb2375898
JohnFreddyTutistar <johnfre.157@gmail.com>
2022-06-30 00:13:38 -0500

    fixed

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@ const ul = document.querySelector("ul")
 const empty = document.querySelector("#empty")
 
 btnTodo.addEventListener("click", (e) => {
+
     const p = document.createElement('p')
     const li = document.createElement('li');
     const input = document.createElement("input");
-    
+
     const text = inputTodo.value;
 
-    
 
-    if(text !== ''){
+
+    if (text !== '') {
         const btnDelete = document.createElement("button");
         btnDelete.className = "btn btn-outline-danger"
         btnDelete.data = "hola";
@@ -25,15 +26,15 @@ btnTodo.addEventListener("click", (e) => {
                 d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1     1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5     3V2h11v1h-11z" />
             </svg>
         `;
-    
+
         empty.style.display = "none"
-        
-    
+
+
         input.type = "checkbox"
         input.className = "form-check-input"
-    
+
         li.className = "list-group-item d-flex justify-content-between align-items-center"
-        
+
         p.textContent = text;
         ul.className = "list-group"
 
@@ -48,25 +49,27 @@ btnTodo.addEventListener("click", (e) => {
 
             const items = document.querySelectorAll("li");
 
-            if(items.length === 0){
+            if (items.length === 0) {
                 empty.style.display = "block";
             }
         });
 
-        
 
-    inputTodo.value = "";
 
-    }else{
+        inputTodo.value = "";
+
+    } else {
         alert("No has agregado ninguna tarea")
     }
 
-    
-    
-    
+
+
+
 
     //evento al input checkbox
-    input.addEventListener('change', function() {
+    input.addEventListener('change', function () {
         p.style.textDecorationLine = this.checked ? "line-through" : "none";
-      });
+    });
+
+
 });
